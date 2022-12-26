c98488de2fda871bb07b28b6b8f066498321fa7a
Nywaa <nywaavr@gmail.com>
2022-12-26 14:13:57 +0700

    Trying string.length and string.slice

--- Javascript-Labs/javascript/script.js
+++ Javascript-Labs/javascript/script.js
@@ -1,4 +1,11 @@
+//Capping user tweet
 var tweet = prompt("Write your tweet here :");
-var maxTweet = 280;
+var maxTweet = 140;
 var tweetLeft = maxTweet - tweet.length;
 console.log("You have used " + tweet.length + " characters, you have " + tweetLeft + " characters left.");
+
+//Slicing user tweet
+var finalTweet = tweet.slice(0, maxTweet);
+
+//Output the final tweet of the user
+console.log(finalTweet);
