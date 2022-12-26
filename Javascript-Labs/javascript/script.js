d758263e93d4ef72ae0bcaf6fd2b0bc64ec0f022
Nywaa <nywaavr@gmail.com>
2022-12-26 18:04:44 +0700

    Trying JS Aritmathic Operator

--- Javascript-Labs/javascript/script.js
+++ Javascript-Labs/javascript/script.js
@@ -1,12 +1,6 @@
-//Asking user to input their name
-var firstName = prompt("Please enter your first name : ");
-var lastName = prompt("Please enter your last name : ");
+//Ask user's dog age
+var dogAge = prompt("Please insert your dog age here : ");
 
-//Converting user first name into capital letters
-var firstName = firstName.slice(0,1).toUpperCase() + firstName.slice(1,firstName.length).toLowerCase();
-console.log(firstName);
-
-
-//Converting user last name into capital letters
-var lastName = lastName.slice(0,1).toUpperCase() + lastName.slice(1, lastName.length).toLowerCase();
-console.log(lastName);
+//Dog age to human age formula 
+var humanAge = ((dogAge-2) * 4) + 21;
+console.log("Your dog age is " + humanAge + " in human age");
