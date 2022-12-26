97d86e55968ce36f24831eba91539c8fb9f61199
Nywaa <nywaavr@gmail.com>
2022-12-26 15:26:53 +0700

    Experiment on string.touppercase and lowercase

--- Javascript-Labs/javascript/script.js
+++ Javascript-Labs/javascript/script.js
@@ -1,11 +1,12 @@
-//Capping user tweet
-var tweet = prompt("Write your tweet here :");
-var maxTweet = 140;
-var tweetLeft = maxTweet - tweet.length;
-console.log("You have used " + tweet.length + " characters, you have " + tweetLeft + " characters left.");
+//Asking user to input their name
+var firstName = prompt("Please enter your first name : ");
+var lastName = prompt("Please enter your last name : ");
 
-//Slicing user tweet
-var finalTweet = tweet.slice(0, maxTweet);
+//Converting user first name into capital letters
+var firstName = firstName.slice(0,1).toUpperCase() + firstName.slice(1,firstName.length).toLowerCase();
+console.log(firstName);
 
-//Output the final tweet of the user
-console.log(finalTweet);
+
+//Converting user last name into capital letters
+var lastName = lastName.slice(0,1).toUpperCase() + lastName.slice(1, lastName.length).toLowerCase();
+console.log(lastName);
