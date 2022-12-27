2615bc993089ecdd99ecbe4861dabf98d13a7584
Nywaa <nywaavr@gmail.com>
2022-12-27 10:48:18 +0700

    Taking a JS function challange

--- Javascript-Labs/javascript/script.js
+++ Javascript-Labs/javascript/script.js
@@ -1,8 +1,21 @@
-//Function to but es degan ijo
-function getEsDegan(money)
+function lifeInWeeks(userAge)
 {
-    var amountEsDegan = Math.floor(money / 3000);
-    console.log("You will get " + amountEsDegan + " amount of es degan with Rp." + money);
+    //Assuming a human will only live until 90 years old 
+    var maxAge = 90;
+
+    //Const for how many months, weeks, days in a year
+    var monthsinYear = 12;
+    var weeksinYear = 52;
+    var daysinYear = 365;
+
+    //Calculating how many time left for user to live
+    var userMonths = ((maxAge * monthsinYear) - (userAge * monthsinYear));
+    var userWeeks = ((maxAge * weeksinYear) - (userAge * weeksinYear));
+    var userDays = ((maxAge * daysinYear) - (userAge * daysinYear));
+    
+    //Telling user the output
+    //Expected output is : You have x days, y weeks, and z months left.
+    console.log("You have " + userDays + " days, " + userWeeks + " weeks, and " + userMonths + " months left.");
 }
 
-getEsDegan(20000);
+lifeInWeeks(56);
