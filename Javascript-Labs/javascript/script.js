704ccf6a3308eb61c3f88ec22959740678be0777
Nywaa <nywaavr@gmail.com>
2022-12-27 18:47:22 +0700

    Trying JS Function Math.random()

--- Javascript-Labs/javascript/script.js
+++ Javascript-Labs/javascript/script.js
@@ -1,10 +1,19 @@
-//Create a function that contain a formula
-//bmi = weight / height^2
-function bmiCalculator(weight, height)
-{
-    var bmi = Math.floor(weight / Math.pow(height,2));
-    return bmi;
+//Lover compatible
+//Generate a random percentage to determine how match their love is lol
+
+var maleName = prompt("Please enter the male name : ");
+var femaleName = prompt("Please enter the female name : ");
+
+function generatePercentage()
+{   
+    //Generate a number with Math.random()
+    //Math.random() only produce a number between 0 and 1
+    //The formula is Math.random() * x, where x is the max number of the desired number          
+    var randomPercentage = Math.random() * 100;
+    
+    //Rounding the value of Math.random so it doesnt have any double value
+    randomPercentage = Math.floor(randomPercentage) + 1;
+    return randomPercentage;
 }
 
-var bmiValue = bmiCalculator(65,1.8);
-console.log(bmiValue);
+console.log("You and your partner is " + generatePercentage() + "% compatible");
