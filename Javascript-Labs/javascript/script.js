d2fca4a0902b274a8882f568a8625d5883841198
Nywaa <nywaavr@gmail.com>
2022-12-27 15:41:27 +0700

    Trying Math.Pow()

--- Javascript-Labs/javascript/script.js
+++ Javascript-Labs/javascript/script.js
@@ -1,21 +1,10 @@
-function lifeInWeeks(userAge)
+//Create a function that contain a formula
+//bmi = weight / height^2
+function bmiCalculator(weight, height)
 {
-    //Assuming a human will only live until 90 years old 
-    var maxAge = 90;
-
-    //Const for how many months, weeks, days in a year
-    var monthsinYear = 12;
-    var weeksinYear = 52;
-    var daysinYear = 365;
-
-    //Calculating how many time left for user to live
-    var userMonths = ((maxAge * monthsinYear) - (userAge * monthsinYear));
-    var userWeeks = ((maxAge * weeksinYear) - (userAge * weeksinYear));
-    var userDays = ((maxAge * daysinYear) - (userAge * daysinYear));
-    
-    //Telling user the output
-    //Expected output is : You have x days, y weeks, and z months left.
-    console.log("You have " + userDays + " days, " + userWeeks + " weeks, and " + userMonths + " months left.");
+    var bmi = Math.floor(weight / Math.pow(height,2));
+    return bmi;
 }
 
-lifeInWeeks(56);
+var bmiValue = bmiCalculator(65,1.8);
+console.log(bmiValue);
