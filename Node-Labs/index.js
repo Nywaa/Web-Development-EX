ed96aa0f539f37cc8617dfe96f3dc4c194f29ee6
Nywaa <nywaavr@gmail.com>
2023-04-01 16:21:15 +0700

    Added NodeJS Labs folder

---
+++ Node-Labs/index.js
@@ -0,0 +1,7 @@
+//npm install pokemon
+const pokemon = require('pokemon');
+
+var pokemon1 = pokemon.random();
+var pokemon2 = pokemon.random();
+
+console.log("Pokemon battle: " + pokemon1 + " vs " + pokemon2);
